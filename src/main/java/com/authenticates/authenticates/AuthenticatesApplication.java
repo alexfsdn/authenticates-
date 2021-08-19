e3ec77ea3448be5bea41c3040470b9350ac37034
Alex Francisco <alexfsdn@gmail.com>
2021-08-19 18:02:09 -0300

    first commit - base project

---
+++ src/main/java/com/authenticates/authenticates/AuthenticatesApplication.java
@@ -0,0 +1,13 @@
+package com.authenticates.authenticates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authenticates/authenticates/AuthenticatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authenticates.authenticates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
